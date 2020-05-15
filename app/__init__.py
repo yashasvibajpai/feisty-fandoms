258f0ca9bcf375df1df4b3bda0dfbf43b2be08b3
yashasvibajpai <yashasvi.bajpai1999@gmail.com>
2020-05-15 18:48:31 +0530

    Revert "add postgres db on a dev branch"
    This reverts commit cc90f9b4d1ad322d513995dbfc7aa7634272f318.

--- app/__init__.py
+++ app/__init__.py
@@ -18,30 +18,30 @@ bootstrap = Bootstrap(app)
 moment=Moment(app)
 
 
-def create_app(config_class=Config):
-    # ...
-    if not app.debug and not app.testing:
-        # ...
+# def create_app(config_class=Config):
+#     # ...
+#     if not app.debug and not app.testing:
+#         # ...
 
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/feistyfandoms.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
+#         if app.config['LOG_TO_STDOUT']:
+#             stream_handler = logging.StreamHandler()
+#             stream_handler.setLevel(logging.INFO)
+#             app.logger.addHandler(stream_handler)
+#         else:
+#             if not os.path.exists('logs'):
+#                 os.mkdir('logs')
+#             file_handler = RotatingFileHandler('logs/feistyfandoms.log',
+#                                                maxBytes=10240, backupCount=10)
+#             file_handler.setFormatter(logging.Formatter(
+#                 '%(asctime)s %(levelname)s: %(message)s '
+#                 '[in %(pathname)s:%(lineno)d]'))
+#             file_handler.setLevel(logging.INFO)
+#             app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('FeistyFandoms startup')
+#         app.logger.setLevel(logging.INFO)
+#         app.logger.info('FeistyFandoms startup')
 
-    return app
+#     return app
 
 # Imported at end to avoid circular import
 from app import routes, models, errors
